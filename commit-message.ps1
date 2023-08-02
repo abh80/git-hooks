831a7e1ddaea76f90b4b6273daf488d6a5218423
abh80 <abhb787@outlook.com>
2023-08-02 16:07:45 +0530

    refactor(commit-message.ps1): added ability to recover commit messages during errors

--- commit-message.ps1
+++ commit-message.ps1
@@ -6,6 +6,40 @@ function GetModifiedFiles {
     git diff --cached --name-status # using git to get all staged commits
 }
 
+function handleSavedFile {
+    param(
+        [string]$savedCommitFile
+    )
+
+    $shouldDo = Read-Host "Found an existing saved commit file from a previously failed sesssion, use it ? (Y/n)"
+    if ($shouldDo -eq "n") {
+        Write-Host "Removing the saved commit file and exiting..."
+        Remove-Item $savedCommitFile
+        exit 0
+    }
+    $content = Get-Content $savedCommitFile -Raw
+    return parseCommitFile -givenString $content
+    
+}
+
+function parseCommitFile {
+    param (
+        [string]$givenString
+    )
+    $splitString = $givenString -split $("-" * 10)
+
+    # Trim the leading/trailing whitespaces of each line
+    $lines = $splitString -replace '^\s+|\s+$' -ne ''
+
+    # Split the lines into two separate arrays
+    $array1 = $lines[0..($lines.Length / 2 - 1)]
+    $array2 = $lines[($lines.Length / 2)..($lines.Length - 1)]
+
+    # Convert the two arrays into the desired format
+    $resultArray = @($array1), @($array2)
+    return $resultArray
+}
+
 function GenerateCommitMessage {
     param (
         [string]$fileStatus
@@ -31,19 +65,34 @@ function GenerateCommitMessage {
     return $prefix + ": " + $message
 }
 
+$savedCommitFile = "$(Get-Location)/.git/SAVED_COMMIT.txt"
+
 $modifiedFiles = @(GetModifiedFiles)
 
+$commitMessage
+
 if ($modifiedFiles.Count -eq 0) {
-    Write-Host "No changes to commit. Exiting."
-    exit 0
+    if (Test-Path -Path $savedCommitFile) {
+        $raw = handleSavedFile -savedCommitFile $savedCommitFile        
+        $commitMessage = $raw[0]
+        $modifiedFiles = $raw[1]
+        
+    }
+    else {
+        Write-Host "No changes to commit. Exiting."
+        exit 0
+    }
 }
+
+$commitStatusJoinDelim = "-" * 10
 
 Write-Host "You will be asked to enter commit details"
 
-$commitMessage = @($modifiedFiles | ForEach-Object {
-        GenerateCommitMessage $_
-    })
-
+if ($null -eq $commitMessage) {
+    $commitMessage = @($modifiedFiles | ForEach-Object {
+            GenerateCommitMessage $_
+        })
+}
 Write-Host "Generated commit message:"
 Write-Host "--------------------------"
 Write-Host $($commitMessage -join "`n")
@@ -54,6 +103,8 @@ if ($shallCommit -eq "n") {
     exit 0
 }
 else {
+    $commitFileContentAsArray = @($commitMessage + $commitStatusJoinDelim + $modifiedFiles)
+    $($commitFileContentAsArray -join "`n") | Out-File -FilePath $savedCommitFile
     $filePaths = @($modifiedFiles | ForEach-Object {
             $fileStatusParts = $_ -split '\s+'
             return $fileStatusParts[1]
@@ -67,14 +118,16 @@ else {
             Invoke-Expression "git add `"$file`"" > $null
             Invoke-Expression "git commit -m `"$cm`"" > $null
             if ($LASTEXITCODE -ne 0) {
-                Write-Host "An error occurred during the commit process. Changes have been reset."
+                Invoke-Expression "git reset" > $null
+                Write-Host "An error occurred during the commit process. Changes have been reset`nYour commit messages are not gone! Run the script again without staging anything to recover the commits."
                 exit 1
             }
         }
     }
     catch {
+        Invoke-Expression "git reset" > $null
         exit 1
     }
 }
-
 Write-Host "Wrote the commit, sucessfully"
+Remove-Item -Path $savedCommitFile
